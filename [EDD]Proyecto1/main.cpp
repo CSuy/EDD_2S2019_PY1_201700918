75d7703b1fec154eab27df68d5dedd1ad3dd59f8
Miriam Leticia Vasquez Siu <cristiansuy98@gmail.com>
2019-08-24 19:38:48 -0600

    Menu

---
+++ [EDD]Proyecto1/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <stdio.h> /*uso para printf y scanf cuando sera necesario */
+#include <stdlib.h>
+using namespace std;
+void insertar_Imagen()
+{
+    string opcion1;
+        system("cls");
+        cout << "---------- MENU PHOTGEN+++ ----------" << endl;
+        cout << "Ingrese el nombre del archivo .csv" << endl;
+        cin >> opcion1;
+}
+
+
+int main()
+{
+    bool menu=false;
+    int opcion1;
+    do
+    {
+        system("cls");
+        cout << "---------- MENU PHOTGEN+++ ----------" << endl;
+        cout << "1. INSERTAR IMAGEN" << endl;
+        cout << "2. SELECCIONAR IMAGEN" << endl;
+        cout << "3. APLICAR FILTROS" << endl;
+        cout << "4. EDICION MANUAL" << endl;
+        cout << "5. EXPORTAR IMAGEN" << endl;
+        cout << "6. REPORTES" << endl;
+        cout << "7. SALIR" << endl;
+        cin >> opcion1;
+        switch(opcion1)
+        {
+        case 1:
+            system("cls");
+            insertar_Imagen();
+            getwchar();
+            getwchar();
+            break;
+        case 2:
+            system("cls");
+            printf("Esta es la seleccion de imagen");
+            getwchar();
+            getwchar();
+            break;
+        case 3:
+            system("cls");
+            printf("Aqui aplico filtros");
+            getwchar();
+            getwchar();
+            break;
+        case 4:
+            system("cls");
+            printf("Aqui hago una edicion manual");
+            getwchar();
+            getwchar();
+            break;
+        case 5:
+            system("cls");
+            printf("aqui vamos a exportar imagen");
+            getwchar();
+            getwchar();
+            break;
+        case 6:
+            system("cls");
+            printf("Aqui creamos reportes");
+            getwchar();
+            getwchar();
+            break;
+        case 7:
+            system("cls");
+            printf("Aqui vamos a cerrar el programa");
+            menu=true;
+            break;
+        }
+
+    }while(menu!=true);
+        return 0;
+}
+
+
